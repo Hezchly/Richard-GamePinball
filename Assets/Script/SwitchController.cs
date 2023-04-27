dee5d560ded241d17d2fed874544ba805aba6cc1
Hezchly <richard.123rics@gmail.com>
2023-04-27 19:38:30 +0700

    Switch, Ramp, Launcher

---
+++ Assets/Script/SwitchController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    private enum SwitchState
+    {
+        Off,
+        On,
+        Blink
+    }
+
+    public Collider Bola;
+    public Material offMaterial;
+    public Material onMaterial;
+
+    private Renderer renderer;
+    private SwitchState state;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        Set(false);
+
+        StartCoroutine(BlinkTimerStart(5));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == Bola)
+        {
+            Toggle();
+        }
+    }
+
+    private void Set(bool active)
+    {
+        if (active == true)
+        {
+            state = SwitchState.On;
+            renderer.material = onMaterial;
+            StopAllCoroutines();
+        }
+        else
+        {
+            state = SwitchState.Off;
+            renderer.material = offMaterial;
+            StartCoroutine(BlinkTimerStart(5));
+        }
+
+    }
+
+    private void Toggle()
+    {
+        if (state == SwitchState.On)
+        {
+            Set(false);
+        }
+        else
+        {
+            Set(true);
+        }
+    }
+
+    private IEnumerator Blink(int times)
+    {
+        state = SwitchState.Blink;
+
+        for (int i = 0; i < times; i++)
+        {
+            renderer.material = onMaterial;
+            yield return new WaitForSeconds(0.5f);
+            Debug.Log("Blinking");
+            renderer.material = offMaterial;
+            yield return new WaitForSeconds(0.5f);
+
+        }
+
+        state = SwitchState.Off;
+
+        StartCoroutine(BlinkTimerStart(5));
+    }
+
+    private IEnumerator BlinkTimerStart(float time)
+    {
+        yield return new WaitForSeconds(time);
+        StartCoroutine(Blink(2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
